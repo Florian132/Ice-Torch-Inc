f1c083a7fd11f2d7e8f1445f7ff613fe7edbb9cd
Elliot <elliot.padazakos@outlook.com>
2020-02-10 15:50:46 +0100

    Project upload 1
    Whole fucking project plebs

---
+++ Shmup test-1/Assets/Scripts/Enemy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 100;
+    private Rigidbody2D rb;
+    public int enemySpeed = -2;
+
+
+    Highscore highscore;
+    public int points;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        highscore = GameObject.FindObjectOfType<Highscore>();
+
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(0, enemySpeed);
+    }
+    public void TakeDamage (int damage)
+    {
+        health -= damage;
+
+        if(health <= 0)
+        {
+            
+            Die();
+        }
+    }
+    void Die()
+    {
+        highscore.addScore(points);
+        Destroy(gameObject);
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Deleter")
+        {
+            Die();
+        }
+    }
+
+}
---
+++ Shmup test-1/Assets/Scripts/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject EnemyPrefab;
+    public float SpawnTime = 1;
+    public Renderer rend;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemies", 0, SpawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnEnemies()
+    {
+
+        var Min_X = transform.position.x - rend.bounds.size.x / 2;
+
+        var Max_X = transform.position.x + rend.bounds.size.x / 2;
+
+        var spawnPoint = new Vector2(Random.Range(Min_X, Max_X), transform.position.y);
+
+        Instantiate(EnemyPrefab, spawnPoint, Quaternion.identity);
+    }
+}
